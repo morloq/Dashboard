d7f81d06b83389efc012ff42350581a70486736b
Sara <sarahoubertz@gmail.com>
2022-07-21 15:18:20 +0200

    Delete JS file content, not needed

--- javaScript/doStuff.js
+++
@@ -1,9 +0,0 @@
-//get projects:
-const calculator = document.querySelector(".project.one").addEventListener("click", () => {
-    onclick="window.location='https://www.facebook.com/pages/Gr81';";
-});
-const etchASketch = document.querySelector(".project.two");
-const signUpForm = document.querySelector(".project.three");
-const rockPaperScissor = document.querySelector(".project.four");
-const firstBigSite = document.querySelector(".project.five");
-
